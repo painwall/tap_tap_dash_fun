05c7921f3e89a33cab60cc90b2311ff624f68bb3
painwall <GoodWin.228@yandex.ru>
2020-01-29 23:20:36 +0300

    1. изменена иерархия 2. добавлена база данных аккаунтов

--- main/objects/add_in_statistics.py
+++ main/objects/add_in_statistics.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def add_in_statistics(table, columns, values):
-    con = sqlite3.connect('statistics.db')
-    cur = con.cursor()
-    cur.execute(f'INSERT INTO {table} {columns} values{values}')
-    con.commit()
+    con_stat = sqlite3.connect('data/statistics.db')
+    cur_stat = con_stat.cursor()
+    cur_stat.execute(f'INSERT INTO {table} {columns} values{values}')
+    con_stat.commit()
